01b24ebc3e889914f68d0cd72f7bb11b315bc2ac
hb2atx <josephkillelea@gmail.com>
2023-12-28 16:36:41 -0600

    commit3

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -11,25 +11,91 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addNode(node) {
+    this.nodes.add(node)
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addNodes(nodeArray) {
+    for(let node of nodeArray){
+      this.addNode(node);
+    }
+  }
 
-  // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  // this function accepts two nodes and updates their adjacent values to include the other node
+  addEdge(node1, node2) {
+    node1.adjacent.add(node2);
+    node2.adjacent.add(node1);
+  }
 
-  // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  // this function accepts two nodes and updates their adjacent values to remove the other node
+  removeEdge(node1, node2) {
+    node1.adjacent.delete(node2);
+    node2.adjacent.delete(node1);
+  }
 
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  // this function accepts a node and removes it from the nodes property, it also updates any adjacency lists that include that node
+  removeNode(node) {
+    for(let node of this.nodes) {
+      if (node.adjacent.has(node)) {
+        node.adjacent.delete(node);
+      }
+    }
+    this.nodes.delete(node);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const visited = [];
+    const result = [];
+
+    function traverse(node){
+      // Add a method of base to class
+      if (!node){
+        return null;
+      }
+      // Add a method to visit nodes
+      visited.add(node);
+      result.push(node.value)
+
+      // Visit the neighbors
+      node.adjacent.forEach(neighbor => {
+        if(!visited.has(neighbor)){
+          return traverse(neighbor);
+        }
+      });
+    }
+    traverse(start);
+
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
-}
+  
+  breadthFirstSearch(start) {
+    // Create an empty queue
+    const queue = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    // visit node
+    visited.add(start);
+
+    // While there is still remaining vertices in queue
+    while (queue.length) {
+      currentNode = queue.shift();
+      result.push(currentNode.value);
+
+      // visit neighbors
+      currentNode.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 
 module.exports = {Graph, Node}
